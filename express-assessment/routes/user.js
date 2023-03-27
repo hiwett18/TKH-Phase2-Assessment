454f08986c62afe30edce90cc02cbd03456799be
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2023-03-26 20:56:26 -0400

    updated file

--- express-assessment/routes/user.js
+++ express-assessment/routes/user.js
@@ -1,86 +1,178 @@
 import express from "express";
+// import { prisma } from "../db/index.js";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-export default function userRouter() {
 const router = express.Router();
+router.get('/', async (req, res) => {
+    const { firstName, lastName } = req.body;
+try {
+    const users = await prisma.user.findMany({
+        where: {
+            firstName: firstName,
+            lastName: lastName
+        }
+    });
 
-// Create the routes here
-app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
+    if (users) {
+        res.status(200).json({
+            success: true,
+            users
+            
+        });
+    };
+} catch (e) {
+    res.status(500).json({
+        success: false,
+        message: "Could not find users"
+    });
+};
+   
+    res.json(users)
+    res.status(200).json({
+        success: true
+      })
+    
   });
 
-app.get('/users/admins', async (req, res) => {
-const users = await prisma.user.findMany();
-res.json(users);
-});
-
-app.post('/users', async (req, res) => {
-const { firstName, lastName, isAdmin, isActive } = req.body;
-const newUser = await prisma.user.create({
-    data: {
-        firstName: "James",
-        lastName: "Smith",
-    },
-});
-// res.status(201);
-// console.log(res.json(newUser));
-
-res.status(201).json({
-    success: true
-  })
-
-});
-
-app.post('/users/admins', async (req, res) => {
-    const { firstName, lastName, isAdmin, isActive } = req.body;
+router.post('/', async (req, res) => {
+    const { firstName, lastName } = req.body;
     const newUser = await prisma.user.create({
         data: {
-        firstName,
-        lastName,
-        isAdmin,
-        isActive,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        },
+    });
+    
+    res.json(newUser)
+    res.status(201).json({
+        success: true,
+       
+      })
+    
+    });
+
+
+router.post('/users/admins', async (req, res) => {
+        const { firstName, lastName, isAdmin, isActive } = req.body;
+        const newUser = await prisma.user.create({
+            data: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                isAdmin: req.body.isAdmin,
+                isActive: req.body.isActive,
+                },
+        });
+        res.json(newUser);
+        });
+
+router.get('/users/admins', async (req, res) => {
+    const { firstName, lastName, isAdmin, isActive } = req.body;
+    const newUser = await prisma.user.findMany({
+        data: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isAdmin: req.body.isAdmin,
+        isActive: req.body.isActive,
         },
     });
     res.json(newUser);
+    res.status(200).json({
+        success: true,
+       
+      })
     });
 
-app.get('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const user = await prisma.user.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.json(user);
-  });
+router.get("/:userId", async (req, res) => {
+        const id = req.params.userId;
 
-app.put('/users/:id', async (req, res) => {
-const { id } = req.params;
-const { firstName, lastName, isAdmin, isActive } = req.body;
-const updatedUser = await prisma.user.update({
-    where: {
-    id: parseInt(id),
-    },
-    data: {
-    firstName,
-    lastName
-    },
-});
-res.json(updatedUser);
-});
+        try {
+            const user = await prisma.user.findFirstOrThrow({
+                where: {
+                    id: parseInt(id)
+                }
+            });
 
-app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedUser = await prisma.user.delete({
-      where: {
-        id: parseInt(id),
-      },
+            if (user) {
+                res.status(200).json({
+                    success: true,
+                    user
+                });
+            };
+        } catch (e) {
+            res.status(500).json({
+                success: false,
+                message: "Could not find user"
+            });
+        };
     });
-    res.json(deletedUser);
-  });
-  return router
-}
+
+router.put("/:userId", async (req, res) => {
+            const id = req.params.userId;
+
+            try {
+                const user = await prisma.user.findFirstOrThrow({
+                    where: {
+                        id: parseInt(id),
+                        userId: req.user.id
+                    }
+                });
+
+                if (user) {
+                    const updatedUser = await prisma.user.update({
+                        where: {
+                            id: parseInt(id)
+                        },
+                        data: {
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName
+                        }
+                    });
+
+                    if (updatedUser) {
+                        res.status(200).json({
+                            success: true
+                        });
+                    } else {
+                        res.status(500).json({
+                            success: false,
+                            message: "Failed to update user"
+                        });
+                    };
+                };
+            } catch (e) {
+                res.status(500).json({
+                    success: false,
+                    message: "Could not find user"
+                });
+            };
+        });
+router.delete("/:userId", async (req, res) => {
+        const id = req.params.userId;
+
+        try {
+                const deleteUser = await prisma.user.delete({
+                    where: {
+                        id: parseInt(id)
+                    }
+                });
+
+                if (deleteUser) {
+                    res.status(200).json({
+                        success: true
+                    });
+                } else {
+                    res.status(500).json({
+                        success: false,
+                        message: "Failed to delete user"
+                    });
+                };
+            } catch (e) {
+            res.status(500).json({
+                success: false,
+                message: "Could not find user"
+            });
+        };
+    });
+export default router;
